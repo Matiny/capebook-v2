e867feedfebf66f87056b486608fc833679f7032
Matiny <matinylouis@gmail.com>
2019-11-02 22:04:01 -0400

    Story update complete

--- routes/media.js
+++ routes/media.js
@@ -23,12 +23,21 @@ router.get('/stories/:storyID', (req, res, next) => {
     req.flash('error', 'Please sign in to edit a Story')
     res.redirect('/signin')
   }
+  
   let theID = req.params.storyID;
   User.findOne({username: req.user.username})
     .then((user) => {
       let editArr = user.media.find(story => `${story._id}` === `${theID}`);
-      res.render("content/editstory", { oneStory: editArr });
 
+      let format = {
+        movie : editArr.format === "Movie" ? "selected" : "",
+        game : editArr.format === "Video Game" ? "selected" : "",
+        comic : editArr.format === "Comic" ? "selected" : "",
+        tv : editArr.format === "TV Show" ? "selected" : "",
+        book : editArr.format === "Book" ? "selected" : "",
+      }
+
+      res.render("content/editstory", { oneStory: editArr, format });
     })
     .catch((err) => {
       console.log(err);
@@ -56,16 +65,16 @@ router.post('/stories', (req, res, next) => {
   }
 });
 
-router.put('/stories/:storyID', (req, res, next)=>{
+router.post('/stories/:storyID', (req, res, next)=>{
   let theID = req.params.storyID;
-
+  let { title, year, format, actor, plot } = req.body;
   if (!title) {
     res.render("content/story", { message: "Please Enter a Title" });
     return;
   } else {
     User.findOne({username: req.user.username})
     .then((user) => {
-      let newArr = user.media.filter(story => story._id !== theID);
+      let newArr = user.media.filter(story => `${story._id}` !== `${theID}`);
       user.media = newArr;
       user.media.unshift({ title, year, format, actor, plot });
       user.save();
